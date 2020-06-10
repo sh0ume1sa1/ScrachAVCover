b310f819b7cf07df2eba0db3bf67dc6e82944a68
saishoumei <miitechjp@gmail.com>
2020-06-10 11:01:55 +0900

    リファクタリング

--- ScrachAVCover/src/Car.py
+++ ScrachAVCover/src/Car.py
@@ -33,14 +33,22 @@ class Car():
     @classmethod
     def get_car_url_forsearch(cls):
         return DML_URL_FORSEARCH.replace('TESTTEST', cls.car_no)
-            
+  
+    @classmethod
+    def get_car_cover_url(cls):
+        return cls.car_cover_url
+        
+    @classmethod
+    def get_car_nm(cls):
+        return cls.car_nm
+    
     @classmethod
     def __init__(cls, car_no):
         cls.car_no = car_no
         cls.car_url_forsearch = cls.get_car_url_forsearch()
 
     @classmethod
-    def get_car_cover_url(cls):
+    def get_car_cover_info(cls):
         try:
             #get the url of dmm
             html = urllib.request.urlopen(cls.car_url_forsearch).read()
@@ -51,35 +59,26 @@ class Car():
         except Exception as e:
             logging.exception(e)
 
-    # match the pattern :
-    # starts with 3~5 letters and followed by numbers with or without Hyphen in front
-    @classmethod
-    def validated_car_no(cls):
-        regex = r'\w{2,5}\D*\d{1,5}$'
-        if not(re.match(regex, cls.car_no)):
-            print (cls.car_no +" not matches!")
-            return False
-        return True
+# match the pattern:
+# starts with 3~5 letters and followed by numbers with or without Hyphen in front
+def validated_car_no(car_no):
+    regex = r'\w{2,5}\D*\d{1,5}$'
+    return re.match(regex, car_no)
 
-def dl_car_cover(cls):
-    dl_as_file =  CAR_COVER_DL_DIR + "//" + car.get_car_no() + car.get_car_cover_ext()
+def dl_car_cover(car):
+    dl_as_file =  CAR_COVER_DL_DIR + "\\" + car.get_car_no() + car.get_car_cover_ext()
+    urllib.request.urlretrieve(car.get_car_cover_url(), dl_as_file)
     
-    urllib.request.urlretrieve(c., dl_as_file)
-    
-
 if __name__ == '__main__':
     args = sys.argv
-    print(len(args))
     if  len(args) != 2:
         print("usage : python car.py car-no")
     else:
         car = Car(args[1])
-        #print(args[1], car.car_url_forsearch)
-        if car.validated_car_no():
-            car.get_car_cover_url()
-            print(car.car_nm, car.car_cover_url)
-            
-            #urllib.request.urlretrieve(cover_url, car.dl_as_file)
+        if validated_car_no(car.get_car_no()):
+            car.get_car_cover_info()
+            print(car.get_car_no()+" from "+car.get_car_cover_url()+"\n")
+            dl_car_cover(car)
         else:
             print("car-no is not properiate")
     
