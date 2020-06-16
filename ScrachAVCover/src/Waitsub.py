50cee14728e811876c1ef864ae14f61ea8336427
saishoumei <miitechjp@gmail.com>
2020-06-16 16:56:15 +0900

    Waitsub 修正

--- ScrachAVCover/src/Waitsub.py
+++ ScrachAVCover/src/Waitsub.py
@@ -18,18 +18,20 @@ import re,sys,logging
 import datetime
 
 SUB_DL_DIR = "..\\sub"
-COMMON_HEAD = "https://www.opensubtitles.org/en/"
-TARGET_MOVIE_LIST = ".\\res\\movie_target.json"
-GMAIL ="kayomii2678@gmail.com"
-SUB_URL_FORSEARCH = "https://www.opensubtitles.org/libs/suggest.php?format=json3&MovieName=TESTTEST%20&SubLanguageID=jpn"
-SUB_URL ="https://www.opensubtitles.org/en/subtitleserve/sub/SUBID"
-MOVIE_URL = "https://www.opensubtitles.org/en/search/sublanguageid-jpn/idmovie-MOVIEID"
-MAIL_TXT ="あなたが待ちに待ったサブタイトル:%s が\nありました! ダウンロードアドレス：%s\n ミー"
+MOVIE_FOR_SEARCH = ".\\res\\movie_for_search.json"
+GMAIL =""
+MAIL_TITLE ="あなたがほしい字幕ができました！"
+MAIL_TXT ="映画: %s "
+COMMON_HEAD="https://www.opensubtitles.org/en/"
+SUB_URL_FORSEARCH="https://www.opensubtitles.org/libs/suggest.php?format=json3&MovieName=TESTTEST&SubLanguageID=jpn"
+SUB_URL="https://www.opensubtitles.org/en/subtitleserve/sub/SUBID"
+SUB_DL_URL="http://dl.opensubtitles.org/en/download/sub/SUBID"
+MOVIE_URL="https://www.opensubtitles.org/en/search/sublanguageid-jpn/idmovie-MOVIEID"
 
 class Subtitle(object):
-    movie_title =""
-    movie_year =""
-    sub_url_forsearch =""
+    movie_title = ""
+    movie_year = ""
+    sub_url_forsearch = ""
     sub_file_name = []
     movie_target = []
     
@@ -65,15 +67,16 @@ class Subtitle(object):
     def get_movie_info(cls):
         try:
             r_get = requests.get(cls.sub_url_forsearch)
+            #print(cls.sub_url_forsearch)
             movies_info = json.loads(r_get.text)
             
             for movie_info in movies_info: 
                 if (cls.movie_year != ''):
                     if (movie_info['year'] == cls.movie_year):
-                        print("===MOVIE NAME:",movie_info['name'],movie_info['year'],sep=' ')
+                        #print("===MOVIE NAME:",movie_info['name'],movie_info['year'],sep=' ')
                         cls.set_movie_object(movie_info)
                 else:
-                    print("===MOVIE NAME:",movie_info['name'],movie_info['year'],sep=' ')
+                    #print("===MOVIE NAME:",movie_info['name'],movie_info['year'],sep=' ')
                     cls.set_movie_object(movie_info)
                     
             #print(cls.movie_target)
@@ -84,32 +87,37 @@ class Subtitle(object):
     def get_sub_info(cls, movie_object):
         rtn=[]
         movie_url = movie_object['movie_url']
-        print(movie_object['total']+' subs')
+        print(movie_object['name'],movie_object['total']+' subs')
         html = urllib.request.urlopen(COMMON_HEAD+movie_url).read()
         soup = BeautifulSoup(html,'html.parser')
         if (int(movie_object['total']) > 1):
             #more than one subtitile
-            print(COMMON_HEAD + movie_url)
-            all_sub = soup.find_all('tr',id=re.compile(r'^name\d{7}$')) #nameXXXXXXX subid 7 digital
+            all_sub = soup.find_all('tr',id=re.compile(r'^name\d{3,9}$')) #nameXXXXXXX subid 3~7 digital
             for one_sub in all_sub:
                 sub_id = one_sub.find_all('td')[0]['id'].replace('main','')
                 subName = one_sub.find_all('td')[0].text.replace('Watch onlineDownload Subtitles Searcher','')
                 uploadYmd = one_sub.find_all('td')[3].find('time').text.split("/")
-                subUrl = one_sub.find_all('td')[4].find('a')['href']
-                rating = one_sub.find_all('td')[5].find('span').text + '/' + one_sub.find_all('td')[5].find('span')['title']
+                #subUrl = one_sub.find_all('td')[4].find('a')['href']
+                subUrl = SUB_DL_URL.replace('SUBID', sub_id)
+                try:
+                    rating = one_sub.find_all('td')[5].find('span').text + '/' + one_sub.find_all('td')[5].find('span')['title']
+                except:
+                    rating = "no vote"
                 single_sub = {
                     "subId": sub_id,
                     "subName": subName,
                     "rating":rating,
-                    "uploadYmd":uploadYmd[2]+uploadYmd[1]+uploadYmd[0],
+                    "uploadYmd":uploadYmd[2]+'/'+uploadYmd[1]+'/'+uploadYmd[0],
                     "subUrl":subUrl
                 }
                 #print(single_sub)
                 rtn.append(single_sub)
         else:
             #only one subtitile, html rendered differently
-            subUrl = soup.find('a',download='download')['href']
             sub_id = soup.find('a',download='download')['data-product-id']
+            #subUrl = soup.find('a',download='download')['href']
+            subUrl = SUB_DL_URL.replace('SUBID', sub_id)
+            
             subName = soup.find('a',download='download')['data-product-name']
             single_sub = {  
                   "subId": sub_id,
@@ -119,12 +127,12 @@ class Subtitle(object):
                   "subUrl":subUrl
             }
             rtn.append(single_sub)
-        print(rtn)
+        #print(rtn)
         return rtn
                       
     @classmethod
     def get_sub_url_forsearch(cls):
-        return SUB_URL_FORSEARCH.replace('TESTTEST', cls.movie_title)
+        return SUB_URL_FORSEARCH.replace('TESTTEST', cls.movie_title.replace(' ','%20'))
   
     @classmethod
     def __init__(cls, movie_title, movie_year=''):
@@ -132,20 +140,35 @@ class Subtitle(object):
         cls.movie_year = movie_year
         cls.sub_url_forsearch = cls.get_sub_url_forsearch()
         
-    def send_mail(self):
-        print("send mail")
-        
-    def edit_target(self,cls):
-        print("send mail")
-        
+def send_mail(sub):
+    for m in sub.movie_target:
+        print(m['name'],m['year'])
+        for s in m['subList']:
+            print("--- ",s['subName'].replace('\n',' '))
+            print("---","uploaded on:",s['uploadYmd'],"rating:", s['rating'],s['subUrl'])
+                  
 if __name__ == '__main__':
     args = sys.argv
     if  len(args) <=1:
-        print("usage : python Waitsub.py movie-name [movie-year]")
+        #print("usage : python Waitsub.py movie-name [movie-year]")
+        #movie_for_search.jsonを読み込み
+        with open(MOVIE_FOR_SEARCH) as json_file:
+            movie_for_search_all = json.load(json_file)
+            for m in movie_for_search_all:
+                if (m['wanted']):
+                    sub = Subtitle(m['name'], m['year'])
+                    sub.get_movie_info()
+                    if (sub.movie_target != []):
+                        send_mail(sub)
+                    else:
+                        print("no results")
+            
     else:
+        #command line for test
         sub = Subtitle(args[1], args[2] if len(args)>=3 else '')
         sub.get_movie_info()
-        #urllib.request.urlretrieve(SUB_URL_DOWLOAD,SUB_DL_DIR )
+        print(sub.movie_target)
+        
 
     
     
